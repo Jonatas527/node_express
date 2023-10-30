9027165f9e0060d95007467b86736b251ee66163
EA - Jônatas Da Silva Siqueira <jonatas_siqueira@educadventista.org>
2023-10-30 14:58:02 -0300

    mais um commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const path =require("path")
 
 const app = express()
-const caminhobase = path.join()
+const caminhobase = path.join(__dirname, "templates")
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("estou funcionando com express")
+    resposta.sendFile("${caminho}/index.html")
 })
 app.listen(3000, () => {
     console.log("servidor funcinando na porta 3000")
